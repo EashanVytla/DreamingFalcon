d59170ada6f4cecd69b69507b451e639d6ee3ec3
shankarkalavakolanu <kvvnbs@gmail.com>
2024-03-11 23:14:11 -0400

    First CSV generated debug data

--- data/DataFormatter.m
+++ data/DataFormatter.m
@@ -6,6 +6,7 @@ barroclock = [];
 barroval = [];
 gpsclock = [];
 gpsval = [];
+actionclock = [];
 
 
 
@@ -21,17 +22,10 @@ for i = 1:8208
     imuval(4,i) = sensorData.x0x2Fmavros0x2Fimu0x2Fdata{i, 1}.LinearAcceleration.X;
     imuval(5,i) = sensorData.x0x2Fmavros0x2Fimu0x2Fdata{i, 1}.LinearAcceleration.Y;
     imuval(6,i) = sensorData.x0x2Fmavros0x2Fimu0x2Fdata{i, 1}.LinearAcceleration.Z;
-    imuval(7,i) = -69;
-    imuval(8,i) = -69;
-    imuval(9,i) = -69;
-    imuval(10,i) = -69;
-    imuval(11,i) = -69;
-    imuval(12,i) = -69;
-    imuval(13,i) = -69;
-    imuval(14,i) = -69;
-    imuval(15,i) = -69;
-    imuval(16,i) = -69;
-    imuval(17,i) = -69;
+    
+    for j = 7:21
+        imuval(j,i) = -69;
+    end
 end
 
 imu = [imuclock;imuval];
@@ -43,23 +37,17 @@ for i = 1:8208
 end
 
 for i = 1:8208
-    magval(1,i) = -69;
-    magval(2,i) = -69;
-    magval(3,i) = -69;
-    magval(4,i) = -69;
-    magval(5,i) = -69;
-    magval(6,i) = -69;
+    for j = 1:6
+        magval(j,i) = -69;
+    end
+
     magval(7,i) = sensorData.x0x2Fmavros0x2Fimu0x2Fmag{i, 1}.MagneticField_.X;
     magval(8,i) = sensorData.x0x2Fmavros0x2Fimu0x2Fmag{i, 1}.MagneticField_.Y;
     magval(9,i) = sensorData.x0x2Fmavros0x2Fimu0x2Fmag{i, 1}.MagneticField_.Z;
-    magval(10,i) = -69;
-    magval(11,i) = -69;
-    magval(12,i) = -69;
-    magval(13,i) = -69;
-    magval(14,i) = -69;
-    magval(15,i) = -69;
-    magval(16,i) = -69;
-    magval(17,i) = -69;
+    
+    for j = 10:21
+        magval(j,i) = -69;
+    end
 end
 
 mag = [magclock;magval];
@@ -70,23 +58,16 @@ for i = 1:3055
 end
 
 for i = 1:3055
-    barroval(1,i) = -69;
-    barroval(2,i) = -69;
-    barroval(3,i) = -69;
-    barroval(4,i) = -69;
-    barroval(5,i) = -69;
-    barroval(6,i) = -69;
-    barroval(7,i) = -69;
-    barroval(8,i) = -69;
-    barroval(9,i) = -69;
+    for j = 1:9
+        barroval(j,i) = -69;
+    end
+
     barroval(10,i) = sensorData.x0x2Fmavros0x2Fimu0x2Fstatic_pressure{i, 1}.FluidPressure_;
     barroval(11,i) = -69;
-    barroval(12,i) = -69;
-    barroval(13,i) = -69;
-    barroval(14,i) = -69;
-    barroval(15,i) = -69;
-    barroval(16,i) = -69;
-    barroval(17,i) = -69;
+    
+    for j = 12:21
+        barroval(j,i) = -69;
+    end
 end
 
 barro = [barroclock;barroval];
@@ -97,16 +78,9 @@ for i = 1:1283
 end
 
 for i = 1:1283
-    gpsval(1,i) = -69;
-    gpsval(2,i) = -69;
-    gpsval(3,i) = -69;
-    gpsval(4,i) = -69;
-    gpsval(5,i) = -69;
-    gpsval(6,i) = -69;
-    gpsval(7,i) = -69;
-    gpsval(8,i) = -69;
-    gpsval(9,i) = -69;
-    gpsval(10,i) = -69;
+    for j = 1:10
+        gpsval(j,i) = -69;
+    end
     gpsval(11,i) = sensorData.x0x2Fmavros0x2Fglobal_position0x2Fraw0x2Ffix{i, 1}.Altitude;
     gpsval(12,i) = sensorData.x0x2Fmavros0x2Fglobal_position0x2Fraw0x2Fgps_vel{i, 1}.Twist.Linear.X;
     gpsval(13,i) = sensorData.x0x2Fmavros0x2Fglobal_position0x2Fraw0x2Fgps_vel{i, 1}.Twist.Linear.Y;
@@ -114,12 +88,35 @@ for i = 1:1283
     gpsval(15,i) = sensorData.x0x2Fmavros0x2Fglobal_position0x2Fraw0x2Fgps_vel{i, 1}.Twist.Angular.X;
     gpsval(16,i) = sensorData.x0x2Fmavros0x2Fglobal_position0x2Fraw0x2Fgps_vel{i, 1}.Twist.Angular.Y;
     gpsval(17,i) = sensorData.x0x2Fmavros0x2Fglobal_position0x2Fraw0x2Fgps_vel{i, 1}.Twist.Angular.Z;
+    
+    for j = 18:21
+        gpsval(j,i) = -69;
+    end
 end
 
 gps = [gpsclock;gpsval];
 
+%Action
+for i = 1:4935
+    actionclock(end +1) = ((sensorData.x0x2Fmavros0x2Ftarget_actuator_control{i, 1}.Header.Stamp.Sec) + ((sensorData.x0x2Fmavros0x2Ftarget_actuator_control{i, 1}.Header.Stamp.Nsec)/10^9)) - sensorData.x0x2Fmavros0x2Ftarget_actuator_control{i, 1}.Header.Stamp.Sec + ((sensorData.x0x2Fmavros0x2Ftarget_actuator_control{i, 1}.Header.Stamp.Nsec)/10^9);
+end
 
-Bigboi = [imu,mag,barro,gps];
+for i = 1:4935
+    for j = 1:17
+        actval(j,i) = -69;
+    end
+    
+actval(18,i) = sensorData.x0x2Fmavros0x2Ftarget_actuator_control{i, 1}.Controls(1);
+actval(19,i) = sensorData.x0x2Fmavros0x2Ftarget_actuator_control{i, 1}.Controls(2);
+actval(20,i) = sensorData.x0x2Fmavros0x2Ftarget_actuator_control{i, 1}.Controls(3);
+actval(21,i) = sensorData.x0x2Fmavros0x2Ftarget_actuator_control{i, 1}.Controls(4);
+
+end
+
+action = [actionclock;actval];
+
+
+Bigboi = [imu,mag,barro,gps,action];
 
 
 
@@ -127,14 +124,14 @@ Bigboi = transpose(Bigboi);
 
 Bigboi = sortrows(Bigboi, 1);
 
-for i = 2:18
+for i = 2:22
     if Bigboi(1,i) == -69
         Bigboi(1,i) = 0;
     end
 end
 
-for i = 2:20754
-    for j = 2:18
+for i = 2:25689
+    for j = 2:22
         if Bigboi(i,j) == -69
             Bigboi(i,j) = Bigboi(i-1,j);
         end
@@ -143,28 +140,34 @@ end
 
 %imu range
 for i = 2:7
-    Bigboi(20755,i) = 25;%max
-    Bigboi(20756,i) = -25;%min
+    Bigboi(25690,i) = 25;%max
+    Bigboi(25691,i) = -25;%min
 end
 
 %mag range
 for i = 8:10
-    Bigboi(20755,i) = 25;%max
-    Bigboi(20756,i) = -25;%min
+    Bigboi(25690,i) = 25;%max
+    Bigboi(25691,i) = -25;%min
 end
 
 %baro
-Bigboi(20755,11) = 101325;%max
-Bigboi(20756,11) = 0;%min
+Bigboi(25690,11) = 101325;%max
+Bigboi(25691,11) = 0;%min
 
 %gps
 
-Bigboi(20755,12) = 50;%max alt
-Bigboi(20756,12) = -50;%min alt
+Bigboi(25690,12) = 50;%max alt
+Bigboi(25691,12) = -50;%min alt
 
 for i = 13:18
-    Bigboi(20755,i) = 1;%max
-    Bigboi(20756,i) = -1;%min
+    Bigboi(25690,i) = 1;%max
+    Bigboi(25691,i) = -1;%min
+end
+
+%Action
+for i = 19:22
+    Bigboi(25690,i) = 1;
+    Bigboi(25691,i) = -1;
 end
 
 %imu stuff
@@ -192,8 +195,21 @@ Bigboi(:,16) = normalize(Bigboi(:,16),'range');
 Bigboi(:,17) = normalize(Bigboi(:,17),'range');
 Bigboi(:,18) = normalize(Bigboi(:,18),'range');
 
+%Action
+Bigboi(:,19) = normalize(Bigboi(:,19),'range');
+Bigboi(:,20) = normalize(Bigboi(:,20),'range');
+Bigboi(:,21) = normalize(Bigboi(:,21),'range');
+Bigboi(:,22) = normalize(Bigboi(:,22),'range');
 
+Bigboi = transpose(Bigboi);
 
+ActBoi = [Bigboi(1,:);Bigboi(19:22,:)];
+
+Bigboi = Bigboi(1:18,:);
+
+csvwrite('C:\Users\kbs_s\Documents\GitHub\DreamingFalcon\data\2023-10-13-07-28-08\states.csv', Bigboi);
+
+csvwrite('C:\Users\kbs_s\Documents\GitHub\DreamingFalcon\data\2023-10-13-07-28-08\actions.csv', ActBoi);
 
 % hz = 1/(Bigboi(200,1) - Bigboi(199,1));
 % 
