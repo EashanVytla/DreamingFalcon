1 - quaternion flip fix aka Eashan being a bot

algorithms/agent.py
@@ -177,8 +177,9 @@ class WorldModel(nn.Module):
                 prior = self.rssm.imagine_with_action(data["action"], init)
 
                 return (
-                    history_states,
-                    self.heads["decoder"](self.rssm.get_feat(prior)).mode()
+                    #history_states,
+                    #self.heads["decoder"](self.rssm.get_feat(prior)).mode()
+                    self.heads["reward"](self.rssm.get_feat(prior))
                 )
 
     def _train(self, data):
@@ -226,7 +227,7 @@ class WorldModel(nn.Module):
                     if name == "decoder":
                         loss = -pred.log_prob(history_states)
                     elif name == "reward":
-                        loss = tools.quat_error(pred, data[name])
+                        loss = tools.quat_error(data[name], pred)
                     assert loss.shape == embed.shape[:2], (name, loss.shape)
                     losses[name] = loss
                 scaled = {
@@ -235,7 +236,6 @@ class WorldModel(nn.Module):
                 }
 
                 model_loss = sum(scaled.values()) + kl_loss
-                print(torch.mean(model_loss))
             metrics = self._model_opt(torch.mean(model_loss), self.parameters())
 
         metrics.update({f"{name}_loss": to_np(loss) for name, loss in losses.items()})

modules/tools.py
@@ -701,7 +701,7 @@ def quat_error(q1, q2):
     # Compute the dot product between q1 and the conjugate of q2
     q_error = torch.abs((q1 * q2_conjugate).sum(dim=-1))
     
-    return q_error
+    return 1 - q_error
 
 def lambda_return(reward, value, pcont, bootstrap, lambda_, axis):
     # Setting lambda=1 gives a discounted Monte Carlo return.

valid_dynamics.py
@@ -9,7 +9,7 @@ import csv
 import os
 
 data_directory = "data/SimulatedData4-10/mixed/valid"
-model_path = "models/SimulatedDataModel4-11-5/checkpoint.pt"
+model_path = "models/SimulatedDataModel4-12/model.pt"
 
 def main():
     print("Staring validation...")
@@ -20,7 +20,7 @@ def main():
 
     obs_space = 15
     act_space = 4
-    batch_size = 512
+    batch_size = 256
     sequence_length = 64
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -50,11 +50,10 @@ def main():
         rewards = rewards.to(configs['device'])
 
         data = {'state': states, 'action': actions, 'reward': rewards}
-        history_states, outputs = model._valid(data)
+        #history_states, outputs = model._valid(data)
+        outputs = model._valid(data)
 
-        print(data['state'].shape)
-
-        error += tools.quat_error(rewards, outputs)
+        error += torch.mean(tools.quat_error(data['reward'], outputs), dim=(0,1))
         print(f"Batch {batch_count} Error: {error}")
         torch.cuda.empty_cache()
 