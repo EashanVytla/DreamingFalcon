non-negative quaternion error fix

modules/tools.py
@@ -699,9 +699,9 @@ def quat_error(q1, q2):
     ], dim=-1)
 
     # Compute the dot product between q1 and the conjugate of q2
-    q_error = torch.abs((q1 * q2_conjugate).sum(dim=-1))
+    q_error = (q1 * q2_conjugate).sum(dim=-1)
     
-    return 1 - q_error
+    return torch.abs(1 - q_error)
 
 def lambda_return(reward, value, pcont, bootstrap, lambda_, axis):
     # Setting lambda=1 gives a discounted Monte Carlo return.